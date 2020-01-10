1ff369a2b670fdc32eba58ea040f53c6202b0995
upup2544 <upup2544@hotmail.com>
2020-01-10 16:08:54 +0700

    1

--- rehearsal_4.cpp
+++ rehearsal_4.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-
-//Write compress() here.
+using namespace std;
+string compress(string x){
+    int i=0;
+    string y;
+    while(i<x.size()){
+        y=y+x[i];
+        i=i+3;
+    }
+return y;
+}
 
 int main()
 {
-    string a = conpress("ABCDEFGHIJKLMN");
-    string b = conpress("123456");
-    string c = conpress("HelloWorld");
-    string d = conpress("BNK48");
-    string e = conpress("COMPROG261102");
-    string f = conpress("A");
-    string g = conpress("AB");
-    string h = conpress("ABC");
-    string i = conpress("ABCD");
+    string a = compress("ABCDEFGHIJKLMN");
+    string b = compress("123456");
+    string c = compress("HelloWorld");
+    string d = compress("BNK48");
+    string e = compress("COMPROG261102");
+    string f = compress("A");
+    string g = compress("AB");
+    string h = compress("ABC");
+    string i = compress("ABCD");
     
-    cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n" << h << "\n" << i << "\n";
-
+    cout <<a<<"\n"<< b<<"\n"<<c<<"\n"<<d<<"\n"<<e<<"\n"<<f<<"\n"<<g<<"\n"<<h<<"\n"<<i<< "\n";
+return 0;
 }
