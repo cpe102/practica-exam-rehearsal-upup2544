efae63bfa88ac979e14f5d6874a33c04625a9c2b
upup2544 <upup2544@hotmail.com>
2020-01-10 14:48:53 +0700

    1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    float y;
+    string x;
+    cout<<"What is your name?:";
+    cin>>x;
+    cout<<"What is your GPA?:";
+    cin>>y;
+if(y<3.50){
+    cout<<"Try harder, "<<x<<"!!!";
+}else{
+    cout<<x<<"InW Jrim Jrim!!!";
+}
+return 0;
+}
