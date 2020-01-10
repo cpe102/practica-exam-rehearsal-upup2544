6c9a9d8ccef6eeedb6968e8ff15a40b9bf9d1be2
upup2544 <upup2544@hotmail.com>
2020-01-10 15:06:01 +0700

    1

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    float n=1,x,sum=0; 
+while(n>0){
+ cout<<"Enter x: ";
+    cin>>x;
+if(x==0){
+    cout<<"sum = "<<sum;
+    return 0;
+}
+    if(x>0){
+        sum=sum+x;
+    }
+    n++;}
+}
